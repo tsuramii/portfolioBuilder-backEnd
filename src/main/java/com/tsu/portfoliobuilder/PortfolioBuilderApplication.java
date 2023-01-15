9f5eb6769f5b429efb9981274a70f027c17b7e4e
Tsu <lucanahtsu@gmail.com>
2023-01-15 11:24:18 -0300

    first commit

---
+++ src/main/java/com/tsu/portfoliobuilder/PortfolioBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.tsu.portfoliobuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsu/portfoliobuilder/PortfolioBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsu.portfoliobuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
