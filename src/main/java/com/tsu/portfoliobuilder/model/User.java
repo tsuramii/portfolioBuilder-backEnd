3cfc7cb38fdfdc5b81f15bfedd637d788c44cd08
Tsu <lucanahtsu@gmail.com>
2023-01-15 11:47:43 -0300

    add user class

---
+++ src/main/java/com/tsu/portfoliobuilder/model/User.java
@@ -0,0 +1,59 @@
+package com.tsu.portfoliobuilder.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    @Size(min = 8, max = 60)
+    private String password;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
