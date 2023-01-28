92cb8e820c2b597923d6f06d0d95a7f727eeafe5
Tsu <lucanahtsu@gmail.com>
2023-01-28 01:21:24 -0300

    add education model

---
+++ src/main/java/com/tsu/portfoliobuilder/model/Education.java
@@ -0,0 +1,80 @@
+package com.tsu.portfoliobuilder.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "Educations")
+public class Education {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String description;
+
+    private String startDate;
+    private String endDate;
+
+    // Constructors
+    public Education() {
+    }
+
+    public Education(int id, String title, String description, String startDate, String endDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // Getter and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+}
