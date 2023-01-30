3734d84734dfff0f5d3e25929b11d7375c6d26e1
Tsu <lucanahtsu@gmail.com>
2023-01-29 22:28:11 -0300

    add repository annotation

--- src/main/java/com/tsu/portfoliobuilder/repository/UserDetailsRepository.java
+++ src/main/java/com/tsu/portfoliobuilder/repository/UserDetailsRepository.java
@@ -1,8 +1,9 @@
 package com.tsu.portfoliobuilder.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.tsu.portfoliobuilder.model.UserDetails;
 
-public interface UserDetailsRepository extends JpaRepository<UserDetails, Long>{
-    
+@Repository
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
 }
