7097dee448482bdf09440d8c4fd9349e4a87a6f0
Tsu <lucanahtsu@gmail.com>
2023-01-29 22:30:56 -0300

    delete a line

--- src/main/java/com/tsu/portfoliobuilder/repository/ApiUserRepository.java
+++ src/main/java/com/tsu/portfoliobuilder/repository/ApiUserRepository.java
@@ -1,7 +1,6 @@
 package com.tsu.portfoliobuilder.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.tsu.portfoliobuilder.model.ApiUser;
