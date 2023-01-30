a3f6b5e202b49ef46153801f668193a65412a062
Tsu <lucanahtsu@gmail.com>
2023-01-29 21:59:51 -0300

    add findByUsername method

--- src/main/java/com/tsu/portfoliobuilder/repository/ApiUserRepository.java
+++ src/main/java/com/tsu/portfoliobuilder/repository/ApiUserRepository.java
@@ -1,10 +1,12 @@
 package com.tsu.portfoliobuilder.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.tsu.portfoliobuilder.model.ApiUser;
 
 @Repository
 public interface ApiUserRepository extends JpaRepository<ApiUser, Long> {
-    ApiUser findByUsername(String username);
+    public Optional<ApiUser> findByUsername(String username);
 }
