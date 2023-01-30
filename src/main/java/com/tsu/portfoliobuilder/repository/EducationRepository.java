a4bb90aeb6292f8702cee55a2adc3d33ec23fbb4
Tsu <lucanahtsu@gmail.com>
2023-01-29 23:19:14 -0300

    add findByTitle method

--- src/main/java/com/tsu/portfoliobuilder/repository/EducationRepository.java
+++ src/main/java/com/tsu/portfoliobuilder/repository/EducationRepository.java
@@ -1,9 +1,11 @@
 package com.tsu.portfoliobuilder.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.tsu.portfoliobuilder.model.Education;
 
 @Repository
 public interface EducationRepository extends JpaRepository<Education, Long> {
+    Optional<Education> findByTitle(String title);
 }
