4f8ee79676230e24bce656b55c7432ba2b8d7db3
Tsu <lucanahtsu@gmail.com>
2023-02-07 08:26:16 -0300

    add Response Entities

--- src/main/java/com/tsu/portfoliobuilder/controller/UserDetailsController.java
+++ src/main/java/com/tsu/portfoliobuilder/controller/UserDetailsController.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,26 +26,38 @@ public class UserDetailsController {
     private UserDetailsService userDetailsService;
 
     @GetMapping("/")
-    public List<UserDetails> findAllUserDetails() {
-        return userDetailsService.findAllUserDetails();
+    public ResponseEntity<List<UserDetails>> findAllUserDetails() {
+        List<UserDetails> userDetails = userDetailsService.findAllUserDetails();
+        return ResponseEntity.ok(userDetails);
     }
 
     @GetMapping("/details/{id}")
-    public Optional<UserDetails> findById(@PathVariable Long id) {
-        return userDetailsService.findById(id);
+    public ResponseEntity<Optional<UserDetails>> findById(@PathVariable Long id) {
+        Optional<UserDetails> userDetails = userDetailsService.findById(id);
+        if (!userDetails.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userDetails);
     }
 
     @PostMapping("/create")
-    public UserDetails saveUserDetails(@RequestBody UserDetails userDetails) {
-        return userDetailsService.saveUserDetails(userDetails);
+    public ResponseEntity<UserDetails> saveUserDetails(@RequestBody UserDetails userDetails) {
+        UserDetails savedUserDetails = userDetailsService.saveUserDetails(userDetails);
+        return ResponseEntity.ok(savedUserDetails);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteById(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         userDetailsService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
-    public UserDetails updateUserDetails(@PathVariable Long id, @RequestBody UserDetails userDetails) {
-        return userDetailsService.updateUserDetails(id, userDetails);
+    @PutMapping("/update/{id}")
+    public ResponseEntity<UserDetails> updateUserDetails(@PathVariable Long id, @RequestBody UserDetails userDetails) {
+        UserDetails updatedUserDetails = userDetailsService.updateUserDetails(id, userDetails);
+        if (updatedUserDetails == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedUserDetails);
     }
 }
